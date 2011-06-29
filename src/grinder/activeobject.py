127c644408e1e8ff2ee30f336f979143f23fbd3a
Jeff Ortel <jortel@redhat.com>
2011-06-29 13:19:58 -0500

    Tunnel logging in child back to parent in activeobject.

--- src/grinder/activeobject.py
+++ src/grinder/activeobject.py
@@ -11,14 +11,27 @@
 
 """
 Active Object classes.
+Protocol:
+ Codes:
+     0 = Normal Return
+         retval = returned value
+     1 = Raised Exception
+         retval = raised exception
+     2 = Logging Request
+         retval = log-record
+ Record:
+   call  = (object, method, *args, **kwargs)
+   log   = (logger, level, msg, args)
+   reply = (code, retval, state)
 """
 
 import sys
 import atexit
+import logging
 import cPickle as pickle
 import traceback as tb
 from subprocess import Popen, PIPE
-    
+
 
 class Method:
     """
@@ -76,9 +89,7 @@ class ActiveObject:
         Method invocation.
         The active object, method name and arguments are picked and
         sent to the child on the stdin pipe.  Then, the result is read
-        on the child stdout pipe. A code=0 indicates that the retval is
-        a normal returned value.  A code=1 indicates that the retval is
-        a rasied exception which is raised.
+        on the child stdout pipe.  See: Protocol.
         @param method: The method name.
         @type method: str
         @param args: The argument list.
@@ -90,12 +101,31 @@ class ActiveObject:
         call = (self.object, method, args, kwargs)
         pickle.dump(call, p.stdin)
         p.stdin.flush()
-        code, retval, state = pickle.load(p.stdout)
-        if code == 0:
-            setstate(self.object, state)
-            return retval
-        else:
-            raise Exception(retval)
+        while True:
+            code, retval, state = pickle.load(p.stdout)
+            if code == 0:
+                setstate(self.object, state)
+                return retval
+            if code == 1:
+                raise Exception(retval)
+            if code == 2:
+                self.__logchild(*retval)
+                
+    def __logchild(self, name, level, msg, args):
+        """
+        Perform child logging request
+        @param name: The logger name.
+        @type name: str
+        @param level: The logging level.
+        @type level: str
+        @param msg: The log message
+        @type msg: str
+        @param args: The argument list
+        @type args: list
+        """
+        log = logging.getLogger(name)
+        method = getattr(log, level)
+        method(msg, *args)
     
     def __spawn(self):
         """
@@ -152,16 +182,118 @@ class ActiveObject:
         """
         Clean up the child process.
         """
-        self.__kill()    
+        self.__kill()
+        
+        
+class Logger:
+    """
+    The remote logging proxy.
+    @ivar name: The logger name.
+    @type name: str
+    """
+    
+    def __init__(self, name):
+        """
+        @param name: The logger name.
+        @type name: str
+        """
+        self.name = name
+
+    class Method:
+        
+        def __init__(self, logger, name):
+            """
+            The logging method stub.
+            @param logger: The logger object.
+            @type logger: L{Logger}
+            @param name: The method name (level).
+            @type name: str
+            """
+            self.logger = logger
+            self.name = name
+        
+        def __call__(self, msg, *args, **kwargs):
+            """
+            Invoke the logging call.
+            @param msg: The message to log.
+            @type msg: object
+            @param args: The argument list
+            @type args: tuple
+            @param kwargs: The keyword args.
+            @type kwargs: dict
+            """
+            msg, args = \
+                self.__processArgs(msg, args)
+            msg, kwargs = \
+                self.__processKeywords(msg, kwargs)  
+            self.__send(msg, args)
+            
+        def __processArgs(self, msg, args):
+            """
+            Process the arguments.
+            When (msg) is an exception, replace it with formatted
+            message and concattenated traceback.
+            @param msg: The message argument.
+            @type msg: object
+            @param args: The argument list.
+            @type args: tuple
+            @return: The processed (msg, args)
+            @rtype: tuple 
+            """
+            arglist = []
+            if isinstance(msg, Exception):
+                msg = '\n'.join((str(msg), trace()))
+            for arg in args:
+                if isinstance(arg, Exception):
+                    arg = '\n'.join((str(arg), trace()))
+                arglist.append(arg)
+            return (msg, arglist)
+            
+        def __processKeywords(self, msg, keywords):
+            """
+            Process the keyword arguments.
+            When 'exc_info' is True, append the traceback information
+            to the returned message.
+            @param msg: The message argument.
+            @type msg: object
+            @param keywords: The keyword arguments.
+            @type keywords: dict
+            @return: The processed (msg, kwargs)
+            @rtype: tuple 
+            """
+            exflag = keywords.pop('exc_info', 0)
+            if exflag:
+                msg = '\n'.join((msg, trace()))
+            return (msg, keywords)
+            
+        def __send(self, msg, args):
+            """
+            Send the logging request to the parent.
+            @type msg: object
+            @param args: The argument list.
+            @type args: tuple
+            """
+            lr = (self.logger.name,
+                  self.name, 
+                  msg,
+                  args)
+            pickle.dump((2, lr, {}), sys.stdout)
+            sys.stdout.flush()
+            
+    def __getattr__(self, name):
+        """
+        @return: The method stub.
+        @rtype: L{Logger.Method}
+        """
+        return self.Method(self, name)
 
     
 def process():
     """
     Reads and processes RMI requests.
-     Input: (object, method, *args, **kwargs)
-    Output: (status, retval, state)
-        status (0=return value, 1=exception).
-        retval (returned value | exception )
+     Input: (object, method, args, kwargs)
+    Output: (code, retval, state)
+    See: Protocol.
     """
     code = 0
     state = {}
@@ -175,29 +307,33 @@ def process():
         state = getstate(object)
     except:
         code = 1
-        info = sys.exc_info()
-        retval = '\n'.join(tb.format_exception(*info))
+        retval = trace()
     result = (code, retval, state)
     pickle.dump(result, sys.stdout)
     sys.stdout.flush()
     
 def getstate(object):
-    M = '__getstate__'
-    if hasattr(object, M):
-        m = getattr(object, M)
-        return m()
+    key = '__getstate__'
+    if hasattr(object, key):
+        method = getattr(object, key)
+        return method()
     else:
         return object.__dict__
     
 def setstate(object, state):
-    M = '__setstate__'
-    if hasattr(object, M):
-        m = getattr(object, M)
-        m(state)
+    key = '__setstate__'
+    if hasattr(object, key):
+        method = getattr(object, key)
+        method(state)
     else:
         object.__dict__.update(state)
+        
+def trace():
+    info = sys.exc_info()
+    return '\n'.join(tb.format_exception(*info))
 
 def main():
+    logging.getLogger = Logger
     while True:
         process()
     
