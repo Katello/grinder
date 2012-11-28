4f599b00027612ebfd16459204adcbc3ddb59ac0
Pradeep Kilambi <pkilambi@redhat.com>
2012-11-28 13:44:41 -0500

    Automatic commit of package [grinder] release [0.1.7-1].

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="grinder",
-    version='0.1.6',
+    version='0.1.7',
     description='A tool for synching RPM based content.',
     author='Mike McCune, John Matthews, Pradeep Kilambi, Jeff Ortel',
     author_email='jmatthews@redhat.com',
